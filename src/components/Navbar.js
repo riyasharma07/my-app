c30051b4d7927ccc3e774f1ce76b6d97af9a9e95
Riya sharma <sharmariyasharma1039@gmail.com>
2021-09-23 23:04:24 +0530

    final

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,9 @@
-import React from 'react'
+import React ,{useState}from 'react'
 import PropTypes from 'prop-types';
 
 export default function Navbar(props) {
+  
+
     return (
         <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
         <div className="container-fluid">
@@ -27,7 +29,8 @@ export default function Navbar(props) {
                 </a>
               </li>
             </ul>
-            
+           
+           
           </div>
         </div>
       </nav>
