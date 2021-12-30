5afaef2a46000d6f7a64ce6acf647ad4f1386b61
riyasharma18 <sharmariyasharma1039@gmail.com>
2021-12-30 16:03:08 +0530

    final

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -25,7 +25,7 @@ export default function Navbar(props) {
             <ul className="navbar-nav me-auto mb-2 mb-lg-0">
               <li className="nav-item">
                 <a className="nav-link active" aria-current="page" href="/">
-                  {props.abouttext}
+                  {/* {props.abouttext} */}
                 </a>
               </li>
             </ul>
